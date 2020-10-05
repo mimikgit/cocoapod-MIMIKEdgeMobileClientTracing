6d769fa00d1197c0149b2ebbd5808a9c15837be2
Raduz Benicky <raduz.benicky@mimik.com>
2020-10-05 09:34:37 -0700

    0.1 Empty release to test the distribution channel.

---
+++ MIMIKEdgeMobileClientTracing.framework/Headers/MIMIKEdgeMobileClientTracing.h
@@ -0,0 +1,19 @@
+//
+//  MIMIKEdgeMobileClientTracing.h
+//  MIMIKEdgeMobileClientTracing
+//
+//  Created by Radúz Benický on 2020-10-05.
+//  Copyright © 2020 mimik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MIMIKEdgeMobileClientTracing.
+FOUNDATION_EXPORT double MIMIKEdgeMobileClientTracingVersionNumber;
+
+//! Project version string for MIMIKEdgeMobileClientTracing.
+FOUNDATION_EXPORT const unsigned char MIMIKEdgeMobileClientTracingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MIMIKEdgeMobileClientTracing/PublicHeader.h>
+
+
